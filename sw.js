3c6cf04f1a60f4a3b07a1878a17a88de998637b3
Calculadora60 <soumajor12@gmail.com>
2026-09-06 12:17:19 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('fin-pwa-v1').then((cache) => {
+    caches.open('fin-pwa-v2').then((cache) => {
       return cache.addAll([
         './index.html',
         './manifest.json',
